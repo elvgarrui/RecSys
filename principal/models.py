a8ab6e8982ea5c1d153840bc704d52bc7790116d
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2017-12-22 08:52:22 +0100

    Libro

--- principal/models.py
+++ principal/models.py
@@ -1,5 +1,5 @@
 from django.core import validators
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator,URLValidator
 from django.db import models
 
 
@@ -14,6 +14,21 @@ class Usuario(models.Model):
     localizacion = models.CharField(null=True, blank=True, max_length=100)
     def __unicode__(self):
         return self.idUsuario
+
+
+class Libro(models.Model):
+    isbn = models.CharField(max_length=30, null=False, blank=False)
+    titulo = models.CharField(max_length=30, null=False, blank=False)
+    autor = models.CharField(max_length=30, null=False, blank=False)
+    ano = models.IntegerField()
+    editor=models.CharField(max_length=30, null=False, blank=False)
+    urlS = models.URLField(validators=[URLValidator()])
+    urlM = models.URLField(validators=[URLValidator()])
+    urlL = models.URLField(validators=[URLValidator()])
+    
+    def __unicode__(self):
+        return self.titulo
+
     
 class Puntuacion(models.Model):
     idUsuario = models.IntegerField(null=True, blank=True, max_length=50)
@@ -21,3 +36,4 @@ class Puntuacion(models.Model):
     puntuacion = models.IntegerField(null=True, blank=True, max_length=2, validators=[MinValueValidator(1), MaxValueValidator(10)])
     def __unicode__(self):
         return self.puntuacion
+    
